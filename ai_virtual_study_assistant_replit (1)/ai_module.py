ca68ad7f53640a74972d59b3cf12da2279f285cf
2303A52183 <2303A52183@sru.edu.in>
2025-10-28 21:31:05 +0530

    Delete ai_virtual_study_assistant_replit (1) directory

--- ai_virtual_study_assistant_replit (1)/ai_module.py
+++
@@ -1,1 +0,0 @@
-<full ai_module.py code>
--- ai_virtual_study_assistant_replit (1)/utils.py
+++
@@ -1,3 +0,0 @@
-def load_sample_notes():
-    return """Software Engineering involves systematic development, testing, and maintenance of software.
-It includes requirements gathering, design, implementation, testing, and maintenance."""
